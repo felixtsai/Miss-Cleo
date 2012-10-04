71f0edebffeb8bcb75bfbb77806e0584b8a81d85
Felix Tsai <felixtsai@yahoo.com>
2012-10-03 17:34:27 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MissCleo::Application.config.secret_token = 'a4489e214ea7db44fd3a5aef55a6c331c7ae459af3f2e0480fa010f678a3178a17c1aa37f3ac889e078222d9540edd4af75749969106f2046e2de6a51fe2e2a4'
