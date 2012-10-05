0afe2f4329bf75117fb9c8b105537bdf81864c89
david@ladowitz.com <david@ladowitz.com>
2012-10-05 14:44:11 -0700

    added validations

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,6 +1,7 @@
 class Guess < ActiveRecord::Base
   attr_accessible :height, :weight, :name
-
+  validates :height, :numericality => { :only_integer => true }
+  validates :weight, :numericality => { :only_integer => true }
 
 end
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ActiveRecord::Base
   attr_accessible :height, :name, :sex, :weight
+  validates :height, :numericality => { :only_integer => true }
+  validates :weight, :numericality => { :only_integer => true }
 end
