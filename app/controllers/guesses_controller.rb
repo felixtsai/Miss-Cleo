450bbd236db2a30eddd64e96412f24b76f00c58b
david@ladowitz.com <david@ladowitz.com>
2012-10-06 15:33:25 -0700

    added index and show views for Guess

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,11 +1,22 @@
 class GuessesController < ApplicationController
+
+  def index
+	@guesses = Guess.all
+  end
+
   def new
   	@guess = Guess.new
   end
 
   def create
 	@guess = Guess.create(params[:guess])
-  	redirect_to new_guess_path
+  	redirect_to @guess
   end
+
+  def show
+  	@guess = Guess.find(params[:id])
+  	@pop_state = PopulationState.find(1)
+  end
+
 
 end
