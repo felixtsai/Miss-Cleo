bcca59bd91d0a0fa451bcc4c5ef2dde9c4a4b837
david@ladowitz.com <david@ladowitz.com>
2012-10-08 15:47:09 -0700

    changed migations

--- db/migrate/20121008184824_change_data_type_for_total_height.rb
+++ db/migrate/20121008184824_change_data_type_for_total_height.rb
@@ -8,8 +8,8 @@ class ChangeDataTypeForTotalHeight < ActiveRecord::Migration
 
   def down
   	change_table :population_states do |t|
-  		t.change :total_male_height, :float
-			t.change :total_female_height, :float
+  		t.change :total_male_height, :integer
+			t.change :total_female_height, :integer
   	end
   end
 end
