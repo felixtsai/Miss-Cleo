7151c847789f945ea7575f56eef23fd6d53c162f
Felix Tsai <felixtsai@yahoo.com>
2012-10-07 22:31:53 -0700

    fixed formulas and the guess views

--- app/models/population_state.rb
+++ app/models/population_state.rb
@@ -7,6 +7,7 @@ class PopulationState < ActiveRecord::Base
   end
 
   def mean(gender, attribute)
+    attribute == :height ? pop_data[gender][attribute]/pop_data[gender][:total].to_f/12.0 :
     pop_data[gender][attribute]/pop_data[gender][:total].to_f
   end
 
@@ -16,33 +17,45 @@ class PopulationState < ActiveRecord::Base
 
   def variance(gender, attribute)
     attribute_collection = []
-    Person.all.each do |person|
-     if person.sex == gender.to_s
-      attribute_collection << person.send(attribute)
-     end
+    if attribute == :height
+      Person.all.each do |person|
+       if person.sex == gender.to_s
+        attribute_collection << person.send(attribute)/12.0
+       end #end if
+      end #end each
+    else
+      Person.all.each do |person|
+       if person.sex == gender.to_s
+        attribute_collection << person.send(attribute)
+       end #end if
+      end #end each
     end
     (1/(pop_data[gender][:total] - 1).to_f)* attribute_collection.inject(0) {|sum, attrib| sum + (attrib - mean(gender, attribute))**2}
-  end
+  end #end variance
 
-  def attr_prob(gender, attribute)
-    (1/Math.sqrt(2*Math::PI*variance(gender, attribute)**2))**((-(6-mean(gender, attribute))**2)/2*variance(gender, attribute))
+  def attr_prob(gender, attribute, guess_attribute)
+    if attribute == :height 
+      (1/Math.sqrt(2*Math::PI*variance(gender, attribute))) * Math.exp((-(guess_attribute/12.0 - mean(gender, attribute))**2)/(2*variance(gender, attribute)))
+    else
+     (1/Math.sqrt(2*Math::PI*variance(gender, attribute))) * Math.exp((-(guess_attribute - mean(gender, attribute))**2)/(2*variance(gender, attribute)))
+    end
   end
 
  
-  def posterior(gender)
-    overall_prob(gender)*attr_prob(gender, :height)*attr_prob(gender, :weight)
+  def posterior(gender, guess_height, guess_weight)
+    overall_prob(gender)*attr_prob(gender, :height, guess_height)*attr_prob(gender, :weight, guess_weight)
   end
 
-  def guess
+  def guess(height, weight)
     if total_males <= 1 || total_females <= 1
       "Not enough information. Please enter more person data."      
-    elsif posterior(:male) > posterior(:female)
-      "Our guess is male."
+    elsif posterior(:male, height, weight) > posterior(:female, height, weight)
+      "male."
     elsif
-      posterior(:female) > posterior(:male)
-     "Our guess is female."
+      posterior(:female, height, weight) > posterior(:male, height, weight)
+     "female."
     else
-      "Equal probability. Flip a coin."
+      "Androgynous. Flip a coin."
     end
   end   
 
